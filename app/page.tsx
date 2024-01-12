d4885520a051795ec87f3622565322bf71a628c1
codingjwp <codingjwp@gmail.com>
2024-01-12 11:51:06 +0900

    feat: header 컴포넌트 생성 및 logo link 생성

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import Button from "@/components/buttons/Button"
-import SvgCollection from "@/components/svgs/SvgCollection"
+import Header from "@/components/header/Header";
 
 export default async function Index() {
   return (
-    <div className="flex gap-2">
-      <Button type="button" size="sm" btnColor="baseDark" textColor="white">
-        <SvgCollection svgShapes="visibility" size={48}/>
-        <span className="sr-only">visibility</span>
-      </Button>
+    <div className="">
+      <Header />
     </div>
   )
 }
