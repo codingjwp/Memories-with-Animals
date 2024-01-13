4056c6107ebe12dac8429ec01d2081a4b9c42a2d
codingjwp <codingjwp@gmail.com>
2024-01-13 09:45:09 +0900

    feat: page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-import Button from "@/components/buttons/Button"
-import SvgCollection from "@/components/svgs/SvgCollection"
+import Input from "@/components/inputs/Input"
 
 export default async function Index() {
   return (
     <div className="flex gap-2">
-      <Button type="button" size="sm" btnColor="baseDark" textColor="white">
-        <SvgCollection svgShapes="visibility" size={48}/>
-        <span className="sr-only">visibility</span>
-      </Button>
     </div>
   )
 }
