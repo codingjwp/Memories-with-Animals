1509c4fec5f6b2075728961dcaedcfe34d50aacc
codingjwp <codingjwp@gmail.com>
2024-01-13 09:38:28 +0900

    feat: input 및 show password 컴포넌트 작성

---
+++ components/inputs/Input.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { ChangeEvent, useRef } from "react";
+import ShowPassword from "./ShowPassword";
+
+type InputProps = {
+  label: string,
+  inputType: 'email' | 'password',
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void
+}
+
+export default function Input({label, inputType, onChange}: InputProps) {
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const typeLabel = inputType === 'email' 
+    ? <label htmlFor={inputType}>{label}</label>
+    : (
+        <div className="flex justify-between">
+          <label htmlFor={inputType}>{label}</label>
+          <ShowPassword passwordRef={passwordRef} />
+        </div>
+      )
+
+
+
+  return (
+    <section className="flex flex-col gap-1">
+      {typeLabel}
+      <input
+        ref={inputType === 'password' ? passwordRef : null} 
+        className="w-[210px] h-10 rounded-2xl px-2"
+        type={inputType} id={inputType} name={inputType}
+        autoComplete={inputType === 'email' ? 'username' : 'curent-password'} 
+        autoFocus={inputType === 'email' ? true : false}
+        onChange={onChange}
+        />
+    </section>
+  )
+}
---
+++ components/inputs/ShowPassword.tsx
@@ -0,0 +1,29 @@
+'use clinet'
+import Button from "../buttons/Button"
+import SvgCollection from "../svgs/SvgCollection"
+import { RefObject, useState } from "react"
+
+type VisibleValue = 'visibility' | 'unvisibility'
+type ShowPwProps = {
+  passwordRef: RefObject<HTMLInputElement>
+}
+
+export default function ShowPassword({passwordRef}: ShowPwProps) {
+  const [visible, setVisible] = useState<VisibleValue>('visibility')
+
+  const ClickChangeVisibility = () => {
+    if (passwordRef.current && passwordRef.current.id === 'password') {
+      passwordRef.current.type = passwordRef.current.type === 'password' ? 'text' : 'password'
+      setVisible((prev) => prev === 'visibility' ? 'unvisibility' : 'visibility');
+    }
+  }
+
+  return (
+    <Button type="button" onClick={ClickChangeVisibility}>
+      <SvgCollection svgShapes={`${visible}`} svgFill="white" size={24} />
+      <span className="sr-only">
+        {visible === 'visibility' ? 'show password' : 'hide passworkd'}
+      </span>
+    </Button>
+  )
+}
