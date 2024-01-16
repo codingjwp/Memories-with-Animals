3643fa1e52c3a4b5bebac0a6164b9dfdf2839929
codingjwp <codingjwp@gmail.com>
2024-01-16 15:46:15 +0900

    fix: button 색상 부분 오류로 인한 수정

--- components/buttons/Button.tsx
+++ components/buttons/Button.tsx
@@ -5,8 +5,7 @@ import cx from 'classnames';
 
 type BtnComponentProps = {
   size?: "sm" | "md" | "lr" | "icon",
-  btnColor?: 'baseLight' | 'specialLight' | 'baseDark' | 'specialDark' | "transparent",
-  textColor?: "white" | "black",
+  btnColor?: 'baseLight' | 'baseDark' | 'specialLight' | 'specialDark' | "transparent",
   textPos?: "vertical" | "horizontal",
   customStyle?: string | null,
   onClick?: (event: MouseEvent<HTMLButtonElement>) => void
@@ -17,25 +16,24 @@ type ButtonAttributes = BtnComponentProps & ButtonHTMLAttributes<HTMLButtonEleme
 export default function Button({
   size='sm',
   btnColor='baseDark',
-  textColor='black',
   textPos='horizontal',
   customStyle = null,
   onClick,
   children
 }: ButtonAttributes) { 
   const isDefaultStyle = customStyle === null;
+  const btnBgColor = `bg-${btnColor}`;
   return (
     <button className={cx( 
       {
         'flex items-center justify-center': isDefaultStyle, 
         'transition-all duration-200 ease-in': isDefaultStyle,
-        'rounded-2xl gap-2 hover:shadow-[inset_4px_4px_10px_rgba(0,0,0,0.6),inset_-4px_-4px_10px_rgba(255,255,255,0.6)]': isDefaultStyle && size !== 'icon',
+        'rounded-2xl gap-2 active:shadow-[inset_4px_4px_10px_rgba(0,0,0,0.6),inset_-4px_-4px_10px_rgba(255,255,255,0.6)]': isDefaultStyle && size !== 'icon',
         'w-28 h-10': isDefaultStyle && size === 'sm',
-        'w-60 h-12': isDefaultStyle && size === 'md',
-        'w-full h-12': isDefaultStyle && size === 'lr',
+        'w-60 h-11': isDefaultStyle && size === 'md',
+        'w-full h-11': isDefaultStyle && size === 'lr',
         'flex-col': textPos === 'vertical',
-        [`bg-${btnColor}`]: true,
-        [`text-${textColor}`]: true,
+        [`${btnBgColor}`]: true,
         [`${customStyle}`]: !isDefaultStyle
       })}
       onClick={onClick}>
