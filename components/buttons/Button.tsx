257032e10538c14588cb662737f81aea535cf028
codingjwp <codingjwp@gmail.com>
2024-01-16 21:39:47 +0900

    feat: button 색상 문제로 인한 수정

--- components/buttons/Button.tsx
+++ components/buttons/Button.tsx
@@ -4,10 +4,10 @@ import { MouseEvent, ButtonHTMLAttributes } from "react";
 import cx from 'classnames';
 
 type BtnComponentProps = {
-  size?: "sm" | "md" | "lr" | "icon",
-  btnColor?: 'baseLight' | 'baseDark' | 'specialLight' | 'specialDark' | "transparent",
+  size: "sm" | "md" | "lr" | "icon",
+  btnColor: 'light' | 'dark' | 'sp-light' | 'sp-dark' | "transparent",
   textPos?: "vertical" | "horizontal",
-  customStyle?: string | null,
+  customStyle?: string | undefined,
   onClick?: (event: MouseEvent<HTMLButtonElement>) => void
 }
 
@@ -15,26 +15,28 @@ type ButtonAttributes = BtnComponentProps & ButtonHTMLAttributes<HTMLButtonEleme
 
 export default function Button({
   size='sm',
-  btnColor='baseDark',
+  btnColor,
   textPos='horizontal',
-  customStyle = null,
+  customStyle = undefined,
   onClick,
   children
 }: ButtonAttributes) { 
-  const isDefaultStyle = customStyle === null;
   const btnBgColor = `bg-${btnColor}`;
   return (
     <button className={cx( 
+      'flex items-center justify-center transition-all duration-200 ease-in',
       {
-        'flex items-center justify-center': isDefaultStyle, 
-        'transition-all duration-200 ease-in': isDefaultStyle,
-        'rounded-2xl gap-2 active:shadow-[inset_4px_4px_10px_rgba(0,0,0,0.6),inset_-4px_-4px_10px_rgba(255,255,255,0.6)]': isDefaultStyle && size !== 'icon',
-        'w-28 h-10': isDefaultStyle && size === 'sm',
-        'w-60 h-11': isDefaultStyle && size === 'md',
-        'w-full h-11': isDefaultStyle && size === 'lr',
+        'rounded-2xl gap-2 active:shadow-[inset_4px_4px_10px_rgba(0,0,0,0.6),inset_-4px_-4px_10px_rgba(255,255,255,0.6)]': size !== 'icon',
+        'w-28 h-10': size === 'sm',
+        'w-60 h-11': size === 'md',
+        'w-full h-11': size === 'lr',
         'flex-col': textPos === 'vertical',
-        [`${btnBgColor}`]: true,
-        [`${customStyle}`]: !isDefaultStyle
+        'bg-baseLight': btnColor === 'light',
+        'bg-baseDark': btnColor === 'dark',
+        'bg-specialLight': btnColor === 'sp-light',
+        'bg-specialDark': btnColor === 'sp-dark',
+        'bg-transparent': btnColor === 'transparent',
+        [`${customStyle}`]: customStyle !== undefined
       })}
       onClick={onClick}>
       {children}
