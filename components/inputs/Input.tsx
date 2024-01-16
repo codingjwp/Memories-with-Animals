9552a70b43b0080c260bffa7d9ed1f629f0f8093
codingjwp <codingjwp@gmail.com>
2024-01-16 21:40:14 +0900

    feat: input 수정 및 confirm 추가

--- components/inputs/Input.tsx
+++ components/inputs/Input.tsx
@@ -2,27 +2,32 @@
 
 import cn from 'classnames';
 import ShowPassword from './ShowPassword';
-import { MouseEvent, useState } from 'react';
+import { ChangeEvent, ForwardedRef, MouseEvent, forwardRef, useState } from 'react';
 
 type BaseInputProps = {
   text: string;
   hideText?: boolean;
   required?: boolean;
-  type: 'email' | 'password';
+  type: 'email' | 'password' | 'confirm';
   hidePassword?: boolean;
   addStyles?: string;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function Input({
+function Input({
   text, 
   hideText = false, 
   required = false, 
   type, 
   hidePassword = false,
-  addStyles
-}: BaseInputProps) {
+  addStyles,
+  onChange
+}: BaseInputProps,
+ref: ForwardedRef<HTMLInputElement>
+) {
   const [visible, setVisible] = useState(false);
-  const isEmailType = type === 'email';
+  const isType = (content: string ) => type === content;
+  const isPasswordType = type === 'password';
   const handleClickVisibleChange = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     setVisible((prev) => !prev);
@@ -43,33 +48,33 @@ export default function Input({
       <ShowPassword visibleValue={visible ? 'unvisibility' : 'visibility'} onClick={handleClickVisibleChange} />
     </div>
 
-  // const 
-
-
   return (
     <section className={cn(
       'flex flex-col gap-[2px] accounts w-[270px]',
       {[`${addStyles}`]: typeof addStyles === 'string' }
     )}>
-      {!isEmailType && hidePassword ? showPasswordLabel : baseLabel}
+      {isType('password') && hidePassword ? showPasswordLabel : baseLabel}
       <input
+        ref={ref}
         className={cn(
           'w-full h-9 px-1',
           'outline-none border-b-2 border-slate-400 bg-transparent',
           'focus:border-baseLight',
           'text-black dark:text-white',
         )}
-        type={!isEmailType && !visible ? `${type}` : 'text'}
+        type={isType('password') && visible ? 'text' : (isType('confirm') ? 'password' : `${type}`)}
         id={type}
         name={type}
-        autoFocus={isEmailType ? true : false}
-        autoComplete={isEmailType ? 'username' : 'curent-password'}
-        aria-describedby={isEmailType ? undefined : 'password-constraint'}
+        autoFocus={type === 'email' ? true : false}
+        autoComplete={type === 'email' ? 'username' : 'curent-password'}
         placeholder={hideText ? `${text}` : undefined}
         required
-        // onChange={}
+        onChange={onChange}
       />
-      {isEmailType ? null : <p className='text-black dark:text-white text-xs'>소문자, 대문자, 8자리 이상 입력해주세요.</p>}
+      {isType('password') ? <p className='text-black dark:text-white text-xs'>소문자, 대문자, 8자리 이상 입력해주세요.</p> : null}
+      {isType('confirm') ? <p className='text-black dark:text-white text-xs'>비밀번호를 재확인 해주요.</p> : null}
     </section>
   )
 }
+
+export default forwardRef<HTMLInputElement, BaseInputProps>(Input);
